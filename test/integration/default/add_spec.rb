e5a7e0350a2e297cc13bb488ea9ef0a5d92d1bb8
Jose Luis Salas <josacar@users.noreply.github.com>
2016-06-23 13:16:22 +0000

    Remove unused stuff

--- test/integration/default/add_spec.rb
+++
@@ -1,7 +0,0 @@
-describe 'resolve record' do
-  it 'resolves it' do
-    require 'glare'
-    destination = Glare.resolve('flywire.cc', 'CNAME')
-    expect(destination).to eq(['flywire.com'])
-  end
-end
