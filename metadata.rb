f8f08a6d2d7259ecc49ef483bf2484b48285e48e
Jose Luis Salas <josacar@users.noreply.github.com>
2016-06-24 09:51:12 +0000

    Add source_url and issues_url

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description 'Installs/Configures glare'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.1'
 
+source_url 'https://github.com/peertransfer/glare-cookbook'
+issues_url 'https://github.com/peertransfer/glare-cookbook/issues'
+
 supports 'debian'
 supports 'ubuntu'
 supports 'centos'
