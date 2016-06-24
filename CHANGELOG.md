d9637a98e285a1c8fa2e56702c9aab36fb667c08
Jose Luis Salas <josacar@users.noreply.github.com>
2016-06-24 08:58:49 +0000

    Add Changelog and supports

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# 0.1.1
+
+- Add default action for record provider
+
+# 0.1.0
+
+- Initial release of glare cookbook with record provider
--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,8 @@ description 'Installs/Configures glare'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.1'
 
+supports 'debian'
+supports 'ubuntu'
+supports 'centos'
+
 gem 'glare'
