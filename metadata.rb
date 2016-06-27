a5d2a5e2509024d5119a8dfb1afe32f6174d8042
Jose Luis Salas <josacar@users.noreply.github.com>
2016-06-27 12:55:34 +0000

    Bump version to upload with knife instead of stove

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'engineering@flywire.com'
 license 'MIT'
 description 'Installs/Configures glare'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.0.0'
+version '1.0.1'
 
 source_url 'https://github.com/peertransfer/glare-cookbook'
 issues_url 'https://github.com/peertransfer/glare-cookbook/issues'
