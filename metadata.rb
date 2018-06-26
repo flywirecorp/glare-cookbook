25dd75fa59da11f46026a80953e8780735e9faab
Jose Luis Salas <33053497+joseluis-fw@users.noreply.github.com>
2018-06-26 13:27:16 +0200

    Bump version to 1.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'engineering@flywire.com'
 license 'MIT'
 description 'Manages Cloudflare resources via API v4'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.1.1'
+version '1.1.2'
 
 source_url 'https://github.com/peertransfer/glare-cookbook'
 issues_url 'https://github.com/peertransfer/glare-cookbook/issues'
