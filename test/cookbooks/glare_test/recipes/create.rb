f5e20de2c6863abdabf6b813289300eb4434ac0a
Jose Luis Salas <josacar@users.noreply.github.com>
2019-08-27 18:13:33 +0200

    SREFTP-2006] Add proxied attribute in record resource

--- test/cookbooks/glare_test/recipes/create.rb
+++ test/cookbooks/glare_test/recipes/create.rb
@@ -3,6 +3,7 @@ node.default['glare'] ||= {}
 glare_record 'flywire.com.cn' do
   content 'flywire.com'
   type 'CNAME'
+  proxied true
 
   email node['glare']['email'] || 'test@email.com'
   auth_key node['glare']['auth_key'] || 'example_auth_key'
