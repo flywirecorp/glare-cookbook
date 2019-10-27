778c52fdc35eb70092766275c0f6816802aa994f
Jose Luis Salas <josacar@users.noreply.github.com>
2019-10-27 21:23:22 +0100

    Update cookbook testing framework

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Flywire'
 maintainer_email 'engineering@flywire.com'
 license 'MIT'
 description 'Manages Cloudflare resources via API v4'
-version '1.3.0'
+version '1.4.0'
 
 source_url 'https://github.com/peertransfer/glare-cookbook'
 issues_url 'https://github.com/peertransfer/glare-cookbook/issues'
@@ -14,4 +14,4 @@ supports 'centos'
 
 chef_version '>= 12.6'
 
-gem 'glare', '~> 0.7'
+gem 'glare', '~> 0.8'
